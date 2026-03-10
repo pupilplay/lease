0fbf8a597eba0229549dfcd61be7485b8ec5555a
pupilplay <2715165284@qq.com>
2026-03-11 00:03:59 +0800

    修复由后端查询后台用户的sql语句字段缺失问题。

--- back-end/web/web-admin/src/main/java/com/study/lease/web/admin/service/impl/LoginServiceImpl.java
+++ back-end/web/web-admin/src/main/java/com/study/lease/web/admin/service/impl/LoginServiceImpl.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.rmi.dgc.Lease;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
